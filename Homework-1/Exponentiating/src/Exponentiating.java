458b13c0c35c0af3ba7380d95986105694da8496
Fed-or <fedor.oryshchuk@gmail.com>
2019-02-24 21:05:32 +0200

    убрал метод Math.pow

--- Homework-1/Exponentiating/src/Exponentiating.java
+++ Homework-1/Exponentiating/src/Exponentiating.java
@@ -10,12 +10,16 @@ public class Exponentiating {
         int number = Integer.parseInt(reader.readLine());
         int power = Integer.parseInt(reader.readLine());
 
-        raiseToPower(number, power);
+        System.out.println("Число " + number + " в квадрате равно: " + raiseToPower(number, 2) + ";");
+        System.out.println("Число " + number + " в кубе равно: " + raiseToPower(number, 3) + ";");
+        System.out.println("Число " + number + " в степени " + power + " равно: " + raiseToPower(number, power) + ";");
     }
 
-    public static void raiseToPower(int base, int exponent) {
-        System.out.println("Число " + base + " в квадрате равно: " + Math.pow(base, 2) + ";");
-        System.out.println("Число " + base + " в кубе равно: " + Math.pow(base, 3) + ";");
-        System.out.println("Число " + base + " в степени " + exponent + " равно: " + Math.pow(base, exponent) + ";");
+    public static int raiseToPower(int base, int exponent) {
+        int result = 1;
+        for (int i = 1; i <= exponent; i++) {
+            result = result * base;
+        }
+        return result;
     }
 }
