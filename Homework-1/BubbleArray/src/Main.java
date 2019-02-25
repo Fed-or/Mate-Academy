caaef5087b92fb1080caa86efd7c61f24150e465
Fed-or <fedor.oryshchuk@gmail.com>
2019-02-25 17:34:51 +0200

    added braces

---
+++ Homework-1/BubbleArray/src/Main.java
@@ -0,0 +1,90 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        int[] unSortArray = new int[1000000];
+
+        for (int i = 0; i < unSortArray.length; i++) {
+            unSortArray[i] = (random.nextInt(100));
+        }
+
+        System.out.println();
+        System.out.println("Array before sorting: ");
+        System.out.println(Arrays.toString(unSortArray));
+
+        long before = System.currentTimeMillis();
+
+        BubbleSort.toBubbleSort(unSortArray);
+        System.out.println();
+
+        System.out.println("after bubble sorting: ");
+        System.out.println(Arrays.toString(unSortArray));
+
+        long after = System.currentTimeMillis();
+        long diff = after - before;
+
+        System.out.println("Time: " + diff + "ms");
+
+        System.out.println();
+
+
+        long before2 = System.currentTimeMillis();
+
+        SelectionSort.selectSort(unSortArray);
+
+        System.out.println("after selectSorting: ");
+        System.out.println(Arrays.toString(unSortArray));
+
+        long after2 = System.currentTimeMillis();
+        long diff2 = after2 - before2;
+
+        System.out.println("Time: " + diff2 + "ms");
+        System.out.println();
+
+
+        long before3 = System.currentTimeMillis();
+
+        QuickSort.quickSort(unSortArray, 0, unSortArray.length - 1);
+
+        System.out.println("after quickly sorting: ");
+        System.out.println(Arrays.toString(unSortArray));
+
+        long after3 = System.currentTimeMillis();
+        long diff3 = after3 - before3;
+
+        System.out.println("Time: " + diff3 + "ms");
+        System.out.println();
+
+
+        long before4 = System.currentTimeMillis();
+
+        InsertSort.toInsertSort(unSortArray);
+
+        System.out.println("after bubble insertSorting: ");
+        System.out.println(Arrays.toString(unSortArray));
+
+        long after4 = System.currentTimeMillis();
+        long diff4 = after4 - before4;
+
+        System.out.println("Time: " + diff4 + "ms");
+        System.out.println();
+
+
+        long before5 = System.currentTimeMillis();
+
+        MergeSort.mergeSort(unSortArray);
+
+        System.out.println("after mergeSorting: ");
+        System.out.println(Arrays.toString(unSortArray));
+
+        long after5 = System.currentTimeMillis();
+        long diff5 = after5 - before5;
+
+        System.out.println("Time: " + diff5 + "ms");
+        System.out.println();
+
+    }
+}
---
+++ Homework-1/BubbleArray/src/QuickSort.java
@@ -0,0 +1,38 @@
+public class QuickSort {
+
+
+    public static void quickSort(int[] array, int left, int right) {  // эту сортировку, честно списал и пока не успел разобраться как работает
+        int index = partition(array, left, right);
+        if (left < index - 1) {
+            quickSort(array, left, index - 1);
+        }
+        if (index < right) {
+            quickSort(array, index, right);
+        }
+    }
+
+    public static int partition(int[] array, int left, int right) {
+        int i = left, j = right;
+        int tmp;
+        int pivot = array[(left + right) / 2];
+
+        while (i <= j) {
+            while (array[i] < pivot) {
+                i++;
+            }
+            while (array[j] > pivot) {
+                j--;
+            }
+
+            if (i <= j) {
+                tmp = array[i];
+                array[i] = array[j];
+                array[j] = tmp;
+                i++;
+                j--;
+            }
+        }
+
+        return i;
+    }
+}
