e0eca9a8faf6f16e91105572b7cf7c28a3b5e32b
Fed-or <fedor.oryshchuk@gmail.com>
2019-02-26 00:06:29 +0200

    rename variable and make reader.close

--- Homework-1/Factorial/src/Factorial.java
+++ Homework-1/Factorial/src/Factorial.java
@@ -9,22 +9,20 @@ public class Factorial {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int number = Integer.parseInt(reader.readLine());
 
-        System.out.println("Факториал числа " + number + ": " + "\n" + fact(number));
+        reader.close();
+
+        System.out.println("Factorial" + number + ": " + "\n" + fact(number));
         System.out.println();
-        System.out.println("Первые " + number + " чисел ряда Фибоначчи: ");
+        System.out.println("Firsts " + number + " numbers Fibonacci: ");
         for (int i = 0; i < number; i++) {
             System.out.println(i + ": " + fibonacci(i));
         }
         System.out.println();
     }
 
-    public static int fact(int numberForFactorial) {
-        int result;
+    public static int fact(int number) {
+        return (number == 1 || number == 0) ? 1 : fact(number - 1) * number;
 
-        result = (numberForFactorial == 1 || numberForFactorial == 0) ? 1 :
-                fact(numberForFactorial - 1) * numberForFactorial;
-
-        return result;
     }
 
     public static int fibonacci(int numbersFibonacci) {
