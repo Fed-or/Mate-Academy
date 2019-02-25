be5970cb9266475a92de605eca38c50ffed192e6
Fed-or <fedor.oryshchuk@gmail.com>
2019-02-25 23:21:33 +0200

    added reader.close(); code style review

--- Homework-1/Ternary/src/Ternary.java
+++ Homework-1/Ternary/src/Ternary.java
@@ -22,10 +22,10 @@ public class Ternary{
 
 
         public static int wonOrLost(int teamOne, int teamTwo, int betTeamOne, int betTeamTwo) {
-            int result_1 = (teamOne > teamTwo) ? 1 : 0;
-            int result_2 = (betTeamOne > betTeamTwo) ? 1 : 0;
-            int result_3 = (result_1 == result_2) ? 1 : 0;
+            int whoIsWinner = (teamOne > teamTwo) ? 1 : 0;
+            int betWinner = (betTeamOne > betTeamTwo) ? 1 : 0;
+            int guessedResult = (whoIsWinner == betWinner) ? 1 : 0;
 
-            return ((teamOne == betTeamOne) && (teamTwo == betTeamTwo)) ? 2 : result_3;
+            return ((teamOne == betTeamOne) && (teamTwo == betTeamTwo)) ? 2 : guessedResult;
         }
     }
