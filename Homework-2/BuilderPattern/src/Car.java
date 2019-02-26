f0d2870958e61a1273abbbc50b08b5fea9c2cc78
Fed-or <fedor.oryshchuk@gmail.com>
2019-02-26 19:49:35 +0200

    to Englis

---
+++ Homework-2/BuilderPattern/src/Car.java
@@ -0,0 +1,11 @@
+public class Car {
+    public class Car {
+        private final Type type;
+        private final int seats;
+        private final Engine engine;
+        private final Transmission transmission;
+        private final Speedometer speedometer;
+
+
+
+}
---
+++ Homework-2/BuilderPattern/src/Speedometer.java
@@ -0,0 +1,3 @@
+public enum Speedometer {
+    ELECTRONIC, ANALOG
+}
