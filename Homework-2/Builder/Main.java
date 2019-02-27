25d57714e5d32ec609ca0bd7004816b16139d041
Fed-or <fedor.oryshchuk@gmail.com>
2019-02-27 22:07:46 +0200

    rebase structure

---
+++ Homework-2/Builder/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Car carOne = new Car.CarBuilder("Renault", "Fluence")
+                .carColor("green")
+                .carEngine("diesel")
+                .carMaxSpeed(200)
+                .carSeats(5)
+                .build();
+        System.out.println(carOne);
+    }
+}
